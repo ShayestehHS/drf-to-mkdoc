5e29d8a1c5cfea03da280517695559b264dad90f
shayestehhs <shayestehhs1@gmail.com>
2025-09-10 19:55:49 +0330

    Narrow exception and improve diagnostics when resolving paths

--- drf_to_mkdoc/utils/commons/operation_utils.py
+++ drf_to_mkdoc/utils/commons/operation_utils.py
@@ -4,7 +4,7 @@ import logging
 from functools import lru_cache
 from typing import Any
 
-from django.urls import resolve
+from django.urls import Resolver404, resolve
 
 from drf_to_mkdoc.utils.commons.path_utils import substitute_path_params
 from drf_to_mkdoc.utils.commons.schema_utils import get_schema
@@ -50,9 +50,11 @@ def extract_viewset_from_operation_id(operation_id: str):
             # For viewsets
             return view_func.cls
 
-    except Exception:
+    except Resolver404:
         logger.exception(
-            f"Failed to resolve path.\nschema_path{path}\ntried_path={resolved_path}\n---"
+            "Failed to resolve path. schema_path=%s tried_path=%s",
+            path,
+            resolved_path,
         )
     else:
         return view_func
